a4fe281df24c9e20e2f5e136d26380f01f5e4ba7
bibsgr <bibeli0802@gmail.com>
2023-10-28 20:13:28 -0400

    feat: add stat item of repository component

---
+++ components/molecules/stat-item/index.tsx
@@ -0,0 +1,20 @@
+import React, { Children } from 'react';
+import { View, StyleSheet } from 'react-native';
+import Label from '../../atoms/label/index';
+import theme from '../../../theme';
+
+const parseThousands = (value) =>
+    value >= 1000 ? `${Math.round(value / 100) / 10}k` : String(value);
+
+const StatItem = ({actionText, value, children }) => {
+  return (
+    <View>
+        <Label style={{fontWeight:"bold", color: "orange"}}>
+          {parseThousands(value)}
+        </Label>
+        <Label>{children}</Label>
+    </View>
+  );
+};
+
+export default StatItem;
