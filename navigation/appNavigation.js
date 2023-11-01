82ffcb123b610a597ed736a45a99d18f18f6285f
bibsgr <bibeli0802@gmail.com>
2023-11-01 13:30:05 -0400

    feat: add login and sign in screens

--- navigation/appNavigation.js
+++ navigation/appNavigation.js
@@ -2,7 +2,9 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from '../components/screens/login/index';
 import Home from '../components/screens/home/index';
+import SignUpScreen from '../components/screens/sign-up/index';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,8 +12,10 @@ const Stack = createNativeStackNavigator();
 export default function AppNavigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
+      <Stack.Navigator initialRouteName='Login'>
+        <Stack.Screen name="Login" options={{headerShown: false}} component={LoginScreen} />
         <Stack.Screen name="Home" options={{headerShown: false}} component={Home} />
+        <Stack.Screen name="SignUp" options={{headerShown: false}} component={SignUpScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
